b0184353bd4c88fa7ba983c0eae76ad208abcaf6
oumaymaazouz <58980203+oumaymaazouz@users.noreply.github.com>
2020-07-29 13:56:47 +0300

    add autoplay to embedded video

--- src/components/VideoEmbed.js
+++ src/components/VideoEmbed.js
@@ -7,7 +7,7 @@ const VideoEmbed = () => {
       title="video"
       width="560"
       height="315"
-      src="https://www.youtube.com/embed/0nHSgqRH_DY"
+      src="https://www.youtube.com/embed/0nHSgqRH_DY?autoplay=1&mute=1"
       frameBorder="0"
       allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
       allowFullScreen
