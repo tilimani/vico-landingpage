f4c422dddd0e47bff78f86342fa29e0a0aa913f1
oumaymaazouz <58980203+oumaymaazouz@users.noreply.github.com>
2020-07-29 19:27:59 +0300

    fix faq title

--- src/components/FAQSection/CustomAccordion.js
+++ src/components/FAQSection/CustomAccordion.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
+import RemoveCircleOutlineOutlinedIcon from "@material-ui/icons/RemoveCircleOutlineOutlined";
 import Typography from "@material-ui/core/Typography";
 
 import { makeStyles } from "@material-ui/core/styles";
@@ -13,9 +14,18 @@ const useStyles = makeStyles(theme => ({
     fontWeight: "bold",
     color: theme.palette.secondary.main
   },
-  expand_icon: {
+  add_icon: {
     color: theme.palette.secondary.light,
     fontSize: 20
+  },
+  minus_icon: {
+    color: theme.palette.primary.main,
+    fontSize: 20
+  },
+  expanded_question: {
+    color: theme.palette.primary.main,
+    fontSize: 16,
+    fontWeight: "bold"
   },
   accordion_wrapper: {
     position: "relative",
@@ -25,18 +35,33 @@ const useStyles = makeStyles(theme => ({
   }
 }));
 const CustomAccordion = props => {
+  const [expanded, setExpended] = useState(false);
   const classes = useStyles();
   const { item } = props;
+  const getExpandIcon = () => {
+    if (expanded) {
+      return <RemoveCircleOutlineOutlinedIcon className={classes.minus_icon} />;
+    }
+    return <AddCircleOutlineOutlinedIcon className={classes.add_icon} />;
+  };
   return (
-    <Accordion className={classes.accordion_wrapper}>
+    <Accordion
+      className={classes.accordion_wrapper}
+      onChange={(e, expanded) => {
+        setExpended(expanded);
+      }}
+      expanded={expanded}
+    >
       <AccordionSummary
-        expandIcon={
-          <AddCircleOutlineOutlinedIcon className={classes.expand_icon} />
-        }
+        expandIcon={getExpandIcon()}
         aria-controls="panel1a-content"
         id="panel1a-header"
       >
-        <Typography className={classes.question}>{item.question}</Typography>
+        <Typography
+          className={expanded ? classes.expanded_question : classes.question}
+        >
+          {item.question}
+        </Typography>
       </AccordionSummary>
       <AccordionDetails>
         <Typography>{item.response}</Typography>
